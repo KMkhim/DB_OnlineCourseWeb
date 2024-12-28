45b567a434614c9cbc06ab76ccd1c5bbde1c7bf8
Korbun_S <cosecggirl@gmail.com>
2024-12-28 14:49:45 +0700

    add footer

---
+++ page/footer.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="footer.css">
+</head>
+<body>
+<header>
+  <!-- Content -->
+</header>
+
+<main>
+  <!-- Content -->
+</main>
+
+<footer class="footer">
+  <div class="footer__addr">
+    <h1 class="footer__logo">Our project</h1>
+        
+    <h2>Contact</h2>
+    
+    <address>
+      5555 Somewhere In KMITL<br>
+          
+      <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+    </address>
+  </div>
+  
+  <ul class="footer__nav">
+    
+    
+    <li class="nav__item nav__item--extra">
+      <h2 class="nav__title">รายชื่อสมาชิก</h2>
+      
+      <ul class="nav__ul nav__ul--extra">
+        <li>
+          <a href="#">น.ส.กอบุญ ศุกระกาญจน์</a>
+        </li>
+        
+        <li>
+          <a href="#">นาย กฤตกร จิตต์กำแหง </a>
+        </li>
+        
+        <li>
+          <a href="#">นาย ก้องกิดาการ บุญศรี  </a>
+        </li>
+        
+        <li>
+          <a href="#"> นาย เจษฎา ชาตาสุข .</a>
+        </li>
+        
+        <li>
+          <a href="#">นาย ธันย์ สุภาพพันธุ์ </a>
+        </li>
+        
+      </ul>
+    </li>
+    
+    
+  </ul>
+  
+  <div class="legal">
+    <p>&copy; Copyright @2024 All rights reserved by: The Providers</p>
+    
+    <div class="legal__links">
+      <span>Made with <span class="heart">♥</span> passion from KM</span>
+    </div>
+  </div>
+</footer>
+
+
+
+</body>
+</html>
